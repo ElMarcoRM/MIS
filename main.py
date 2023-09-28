356dcfc6f5cea2872a267dd0379ef050b40f35a2
LisafevaTamara <chupina02@bk.ru>
2023-09-28 21:56:24 +0300

    add_db

--- main.py
+++ main.py
@@ -1,52 +1,40 @@
 from tkinter import *
+import psycopg2
 
+try:
+    connection = psycopg2.connect(
+        host="localhost",
+        user="postgres",
+        password="qwerty",
+        database="db_name"
+    )
+    cur = connection.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS testdata
+              (login TEXT,
+              password TEXT); 
+              ''')
+    connection.commit()
+    print("Таблицы создана")
+
+except Exception as _ex:
+    print("[INFO] Error while working with PostgreSQL", _ex)
+
+
+root = Tk()
+root.title('My App')
+root.geometry('400x400')
+login = Entry(root)
+login.place(x=10, y=90) 
+passwd = Entry(root)
+passwd.place(x=10, y=150)
 def clicked():
-    print('Я же сказал...')
+    name=login.get()
+    password=passwd.get()
+    print(name, password)
+    cur.execute("INSERT INTO testdata(login, password) VALUES ('"+name+"', '"+password+"')")
+    connection.commit()
+    print('Данные внесены')
+lbl1 = Label(root, text='Войти').place(x=10, y=10)
+btn1 = Button(root, text='Войти', command=clicked).place(x=10, y=200)
 
-if __name__ == '__main__':
-    root = Tk()
-    root.title('My App')
-    root.geometry('400x400')
-
-    lbl1 = Label(root, text='...')
-    btn1 = Button(root, text='Не нажимай', command=clicked)
-    entry1 = Entry(root) #creates an entry widget 
-
-    lbl1.place(x=10, y=10)
-    btn1.place(x=10, y=50)
-    entry1.place(x=10, y=90) #place the entry w 
-
-    root.mainloop()
-
-class wid_prm():
-    x = 10
-    y1 = 10
-    y2 = 30
-    y3 = 50
-    x_y_z = 410
-    for auto_prm in range(5):
-        y1 = 10
-        y1 += 20
-        y1 -= 10
-        y1 -= 10
-        if y1 == 30:
-            y1 = 30
-        else:
-            y2 = y1 + 20
-            y3 = y1 + 40
-            y4 = y1 + 60
-            y_values = [y1 * 2 + (y1 * 4)] * 4
-            y5, y6, y7, y8 = y_values
-            y990 = 4
-            if y990 > 1:
-                x = 10
-            else:
-                x = 5
-
-class prm_x_y():
-    x_x = 10
-    y1_y = 50
-
-
-
-#плейсы
+root.mainloop()
